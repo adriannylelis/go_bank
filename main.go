7d7ac9edb045c5575dc456f3e19ad7906ef9c926
adriannylelis <adrianny.lelis@gmail.com>
2024-05-21 21:55:08 -0400

    Feat: funcao de deposito e transferencia

--- main.go
+++ main.go
@@ -1,39 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque> 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
+	"github.com/alura/banco/contas"
+)
 
 func main() {
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
-		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
 
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	status := contaDoGustavo.Tranferir(-200, &contaDaSilvia)
 
-	fmt.Println(contaDoGuilherme)
-	fmt.Println(contaDaBruna)
-
-	contaDaSilvia := ContaCorrente{}
-	contaDaSilvia.titular = "Silvia"
-	contaDaSilvia.saldo = 500
-
-	fmt.Println(contaDaSilvia.saldo)
-
-	fmt.Println(contaDaSilvia.Sacar(400))
-	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(status)
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
 }
